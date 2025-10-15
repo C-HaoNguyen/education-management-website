a46a9990909099392878ee702b459aeae53bf823
Dao Cao <dao.thi.anh.cao@mgm-tp.com>
2025-10-15 20:57:55 +0700

    Add filter for Course Table

--- src/components/CoursesComponent.tsx
+++ src/components/CoursesComponent.tsx
@@ -13,19 +13,29 @@ export default function CourseComponent() {
         description: "Default Courses"
     }]);
     const [newCourseName, setNewCourseName] = useState("");
+    const [filterValue, setFilterValue] = useState("");
 
 
     useEffect(() => {
         fetch("http://localhost:8080/courses/all")
             .then(response => response.json())
             .then(data => {
+                // sort part
                 const sortedCourses = [...data].sort((a, b) =>
                     a.description.localeCompare(b.description)
                 );
-                setCourses(sortedCourses)
+                // filter part
+                if (filterValue === "") {
+                    setCourses(sortedCourses);
+                } else {
+                    const filteredCourses = sortedCourses.filter(value => value.description.includes(filterValue));
+                    setCourses(filteredCourses)
+                }
+
             })
             .catch(error => console.error("Error:", error));
-    }, []);
+            
+    }, [filterValue]); //dependency object
 
     function checkTimeValue(event: ChangeEvent<HTMLInputElement>) {
         const newValue = event.target.value;
@@ -141,6 +151,31 @@ export default function CourseComponent() {
             {/* Header */}
             <div className="flex justify-between items-center mb-6">
                 <h1 className="text-2xl font-bold">Course Management</h1>
+                {/* Thanh tìm kiếm (optional) */}
+                <div className="hidden md:block">
+                    <div className="relative w-128">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="w-full h-10 px-3 py-1 rounded bg-white text-black border-gray focus:outline-blue-500"
+                            onChange={(e) => setFilterValue(e.target.value)}
+                        />
+                        <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-5 w-5 absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z"
+                            />
+                        </svg>
+                    </div>
+                </div>
                 <button className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700" onClick={() => {
                     setShowAddModal(true);
                 }}>
--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -8,31 +8,6 @@ export default function HeaderComponent() {
                     Education Management
                 </div>
 
-                {/* Thanh tìm kiếm (optional) */}
-                <div className="hidden md:block">
-                    <div className="relative w-128">
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            className="w-full h-10 px-3 py-1 rounded bg-white text-black border-gray focus:outline-blue-500"
-                        />
-                        <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-5 w-5 absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z"
-                            />
-                        </svg>
-                    </div>
-                </div>
-
                 {/* User info */}
                 <div className="flex items-center gap-3">
                     <span className="hidden sm:block">Hi, John</span>
